4a71950aff4befdea77938e95baba28472f86bb6
tuckerisapizza <135559488+tuckerisapizza@users.noreply.github.com>
2024-07-19 20:19:50 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,5 @@
 PluginManager.setup($plugins);
 
 window.onload = function() {
-    SceneManager.run(Scene_Boot);
-
-    const canvas = document.getElementById('GameCanvas');
-    canvas.width = 75%;
-    canvas.height = 75%;
-        
+    SceneManager.run(Scene_Boot); 
 }
